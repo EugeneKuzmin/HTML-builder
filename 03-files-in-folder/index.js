c8e62e97f3d22da609baa34f78a1b22a060a1cac
Gennarro@yandex.ru <Gennarro@yandex.ru>
2024-01-22 22:52:08 +0300

    fix:06-build-page  redo async

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,17 +1,3 @@
-// const fs = require('fs');
-// fs.readdir('./03-files-in-folder/secret-folder',
-//     { withFileTypes: true },
-//     (err, files) => {
-//     console.log("\nCurrent directory files:");
-//     if (err)
-//       console.log(err);
-//     else {
-//       files.forEach(file => {
-//         console.log(file);
-//       })
-//     }
-//   })
-
 const fs = require('fs');
 const path = require('path');
 
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -57,3 +57,62 @@ async function generateHTML() {
 }
 
 generateHTML();
+
+
+// CSS
+
+const stylesFolder = './06-build-page/styles';
+const outputFileCSS = './06-build-page/project-dist/bundle.css';
+
+async function combineCSS(files) {
+  const cssContents = await Promise.all(files.map(async (cssFile) => {
+    const filePath = path.join(stylesFolder, cssFile);
+    return await fs.readFile(filePath, 'utf-8');
+  }));
+
+  return cssContents.join('\n');
+}
+
+async function generateCSS() {
+  try {
+
+    const cssFiles = (await fs.readdir(stylesFolder)).filter(file => file.endsWith('.css'));
+
+    const cssContent = await combineCSS(cssFiles);
+
+    await fs.writeFile(outputFileCSS, cssContent, 'utf-8');
+
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+generateCSS();
+
+// assets
+
+async function copyFolder(sourceDir, destinationDir) {
+  try {
+    await fs.mkdir(destinationDir, { recursive: true });
+
+    const files = await fs.readdir(sourceDir);
+
+    await Promise.all(files.map(async (file) => {
+      const sourcePath = path.join(sourceDir, file);
+      const destinationPath = path.join(destinationDir, file);
+
+      const isDir = (await fs.stat(sourcePath)).isDirectory();
+
+      if (isDir) {
+        await copyFolder(sourcePath, destinationPath);
+      } else {
+        await fs.copyFile(sourcePath, destinationPath);
+      }
+    }));
+
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+copyFolder('./06-build-page/assets', './06-build-page/project-dist/assets');
