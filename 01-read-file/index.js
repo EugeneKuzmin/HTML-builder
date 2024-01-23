8f7184cb7b849011b6d3de046c3805944998782b
Gennarro@yandex.ru <Gennarro@yandex.ru>
2024-01-23 11:30:33 +0300

    fix:end prefix expelled from 01-read-file script

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,6 +3,6 @@ const readableStream = fs.createReadStream("./01-read-file/text.txt","utf-8");
 
 let txt = "";
 readableStream.on("data", (chunk) => txt += chunk);
-readableStream.on("end", () => console.log("End", txt));
+readableStream.on("end", () => console.log(txt));
 
 readableStream.on("error", (error) => console.log("Error", error.message));
